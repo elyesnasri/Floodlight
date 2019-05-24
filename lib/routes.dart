a74c7fd4536ae9f4da997ef332d758f1931b29ed
Marco Edenhofer <edenhofermarco@t-online.de>
2019-05-24 14:39:01 +0200

    added class Routes in routes.dart for navigation

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String splashScreen = "/";
+  static const String homeScreen = "/home";
+  static const String detailScreen = "/detail";
+  static const String registerDriverScreen = "/register-driver";
+  static const String registerPassengerScreen = "register-passenger";
+}
